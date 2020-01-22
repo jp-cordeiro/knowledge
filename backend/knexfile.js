f5f5749b905f337e5b5314495c03f7d64264f350
jp-cordeiro <jpc.araujo@hotmail.com>
2020-01-21 22:40:53 -0300

    important data in .env file

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,12 +1,10 @@
 // Update with your config settings.
 
+const { db } = require('./.env')
+
 module.exports = {
   client: 'postgresql',
-  connection: {
-    database: 'knowledge',
-    user:     'postgres',
-    password: ''
-  },
+  connection: db,
   pool: {
     min: 2,
     max: 10
