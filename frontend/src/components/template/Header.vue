2dc629e8fdc662b7c3115ddbf2836816a30bb20a
jp-cordeiro <jpc.araujo@hotmail.com>
2020-01-23 22:06:01 -0300

    main components

---
+++ frontend/src/components/template/Header.vue
@@ -0,0 +1,69 @@
+<template>
+    <header class="header">
+        <a class="toggle" @click="toggleMenu" v-if="toggleMenu">
+            <i class="fa fa-lg" :class="icon"></i>
+        </a>
+        <h1 class="title">
+            {{title}}
+        </h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+        hideToggle: Boolean
+    },
+    computed: {
+        icon(){
+            return 'fa-angle-left'
+        }
+    },
+    methods: {
+        toggleMenu(){
+            
+        }
+    }
+}
+</script>
+
+<style>
+    .header{
+        grid-area: header;
+        background: linear-gradient(to right, #1e469a, #49a7c1);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .title{
+        font-size: 1.2rem;
+        color: #fff;
+        font-weight: 100;
+        flex-grow: 1;
+        text-align: center;
+    }
+
+    .title a{
+        color: #fff;
+        text-decoration: none;
+    }
+
+    .header.header > a.toggle {
+        width: 60px;
+        height: 100%;
+        color: #fff;
+        justify-self: flex-start;
+        text-decoration: none;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    header.header > a.toggle:hover{
+        background-color: rgba(0, 0, 0, .2)
+    }
+</style>
